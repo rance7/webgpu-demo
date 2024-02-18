5b85ff13e02c92a7eeb311bcaf7a1cf2b0926eb4
rance <Rance.ZL.Ji@gmail.com>
2024-02-18 10:39:35 +0800

    init commit

---
+++ webpack.config.mjs
@@ -0,0 +1,51 @@
+import * as path from 'node:path';
+import * as url from 'node:url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const config = {
+    mode: 'development',
+    target: 'web',
+    entry: [
+        './src/app/main.ts',
+        // './src/styles/style.scss',
+    ],
+    devServer: {
+        static: false,
+        watchFiles: ['src/**/*'],
+        liveReload: true,
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /.ts$/u,
+                use: 'ts-loader',
+                exclude: /node_modules/u,
+            },
+            // {
+            //     test: /.scss$/ui,
+            //     use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            // },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        clean: true,
+        hashDigest: 'hex',
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), 'dist'),
+    },
+    plugins: [
+        // new MiniCssExtractPlugin({
+        //     filename: 'bundle.[contenthash].css',
+        // }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ]
+};
+
+export default config;
